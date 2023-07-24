0b34bc7eb19f8104f6a74bf24f4304d0a7fd718a
Bishawnath Ray <bishawnathprogram07@gmail.com>
2023-07-24 17:38:36 +0600

    connect to database

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+ const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+ app.use(cors());
+ app.use(express.json());
+
+
+
+//  const uri = 'mongodb+srv://campusBooking:yXIHY5TQ9JC1cgQg@cluster0.eo0io7y.mongodb.net/?retryWrites=true&w=majority';
+ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eo0io7y.mongodb.net/?retryWrites=true&w=majority`;
+ 
+ // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+ const client = new MongoClient(uri, {
+   serverApi: {
+     version: ServerApiVersion.v1,
+     strict: true,
+     deprecationErrors: true,
+   }
+ });
+ 
+ async function run() {
+   try {
+     // Connect the client to the server	(optional starting in v4.7)
+     await client.connect();
+     // Send a ping to confirm a successful connection
+
+     const allColleges = client.db("eCommerceSite").collection("allColleges");
+     const research = client.db("eCommerceSite").collection("research");
+     const applyForAdmission = client.db("eCommerceSite").collection("applied");
+     const UserCollection = client.db("eCommerceSite").collection("users");
+ 
+
+
+
+
+
+     app.get("/allCollege", async (req, res) => {
+        const result = await allColleges.find().toArray();
+        res.send(result);
+      });
+
+
+
+
+
+
+
+
+
+     await client.db("admin").command({ ping: 1 });
+     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   } finally {
+     // Ensures that the client will close when you finish/error
+    //  await client.close();
+   }
+ }
+ run().catch(console.dir);
+ 
+
+
+ app.get('/', (req , res) =>{
+    res.send("Simple crud is running on collage")
+ })
+
+ app.listen(port , ()=>{
+    console.log(`simple crud is runnig on port , ${port}`)
+ })
